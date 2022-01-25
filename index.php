f153208cf0b4259a537ca27621685d790a8f2e67
Anton <khebnev.a.v@gmail.com>
2022-01-25 22:40:25 +0200

    Добавил index, templates с шапкой, картинку и пустые стили

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>DEKS</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <?php require_once('teplates/header.phtml'); ?>
+
+</body>
+</html>
+<?php
+
